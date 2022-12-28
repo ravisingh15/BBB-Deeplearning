0eed8cf3cf8255f35dbdc229a5d1bb9131611473
Ravi Singh <60500638+ravisingh15@users.noreply.github.com>
2022-12-28 11:56:12 +0000

    t5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import selfies as sf
 import streamlit as st
 import pandas as pd
+
+st.title('BBB Prediction')
+st.header('NLP based deep-learning model to predict the Blood-Brain-Barrier Permeability of drugs')
+
+##Taking input 
+smiles=st.text_input('Please ender smiles of compounds')
+st.write(smiles)
