e9d82ffbf13d22ebd750d1e9bd3a04bf38ad321e
Ravi Singh <60500638+ravisingh15@users.noreply.github.com>
2022-12-30 21:10:21 +0000

    Beta release commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,14 @@ st.header('NLP based deep-learning model to predict the Blood-Brain-Barrier Perm
 #Cc1ccccc1
 ##Taking input
 with st.form(key='input_form'):
-	smiles = st.text_input(label='Please enter SMILES')
+	smiles = st.text_input(label='Please enter CANONICAL SMILES')
 	submit_button = st.form_submit_button(label='Submit')
 	m = Chem.MolFromSmiles(smiles)
 	im=Draw.MolToImage(m)
 	st.image(im)
 	st.write('Generated structure')
 	selfie=sf.encoder(smiles)
-st.write(selfie)
+#st.write(selfie)
 #pre-processing the selfies
 sel_pre_proc=selfie.replace('][',' ').replace(']','').replace('[','')
 a=[sel_pre_proc]
